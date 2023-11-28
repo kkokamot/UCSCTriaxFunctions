e134697cb0c7d6d820377479d6094e264a482bdb
Kristina Okamoto <kokamoto@ldeo.columbia.edu>
2023-11-28 12:56:58 -0800

    update

--- find_holds.m
+++ find_holds.m
@@ -10,8 +10,8 @@ function find_holds(file_df,acc_limit,fig_num, save_file)
             return
         end
     end
-
-
+    
+    % Automatically find holds
     %figure(fig_num)
     disp_d = resample(file_df.LoadingPlattenDispHighGain*1000,1,100);
     Time_d = resample(file_df.Time, 1, 100);
@@ -30,7 +30,6 @@ function find_holds(file_df,acc_limit,fig_num, save_file)
     start_friction = friction_d(acceleration < -acc_limit);
     end_friction = friction_d(acceleration > acc_limit);
 
-
     figure(fig_num)
     plot(file_df.Time, file_df.friction, T_hold, start_friction, 'o', T_end, end_friction, 'o')
     title('Cut Time')
@@ -46,7 +45,7 @@ function find_holds(file_df,acc_limit,fig_num, save_file)
     plot(file_df.Time, file_df.friction, T_hold_f, start_friction_f,'o')
     title('Remove start of hold bad picks (press enter when done)')
     ylim_diff = 2*(max(start_friction_f)-min(end_friction_f));
-    ylim([max(file_df.friction) - ylim_diff, max(file_df.friction)])
+    ylim([max(start_friction_f) - ylim_diff, max(start_friction_f)])
     [x_bad1, y_bad1] = ginput();
     if x_bad1
         for j = 1:length(x_bad1)
@@ -63,7 +62,7 @@ function find_holds(file_df,acc_limit,fig_num, save_file)
     plot(file_df.Time, file_df.friction, T_end_f, end_friction_f,'o')
     xlabel('Time (s)')
     ylabel('\mu')
-    ylim([max(file_df.friction) - ylim_diff, max(file_df.friction)])
+    ylim([max(start_friction_f) - ylim_diff, max(start_friction_f)])
     title('Remove end of hold bad picks (press enter when done)')
     [x_bad2, y_bad2] = ginput();
     if x_bad2
@@ -82,27 +81,53 @@ function find_holds(file_df,acc_limit,fig_num, save_file)
     start_hold_disp = zeros(1, length(T_hold_f)); end_hold_disp = zeros(1, length(T_hold_f));
     for i = 1:length(T_hold_f)
         hold_indices = (file_df.Time > T_hold_f(i)-10 & file_df.Time < T_end_f(i)+100);
-        hold_df = file_df(hold_indices,:);
+        start_hold_indices = (file_df.Time > T_hold_f(i)-10 & file_df.Time < T_hold_f(i)+30);
+        end_hold_indices = (file_df.Time > T_end_f(i)-10 & file_df.Time < T_end_f(i)+30);
+
+        start_df = file_df(start_hold_indices,:);
+        end_df = file_df(end_hold_indices,:);
+
         f = figure();
         f.WindowState = 'maximized';
-        plot(file_df.Time(hold_indices), file_df.friction(hold_indices))
+        subplot(1,3,1)
+        plot(file_df.Time(start_hold_indices), file_df.friction(start_hold_indices))
         hold on
         xlabel('Time (s)')
-        ylabel('Friction (\mu)')
-        title('Pick start and end of hold')
-        clear Time_2 mu_2 I_hold
-        [Time_2, mu_2] = ginput(2);
-        [~,~, I_hold] = after_ginput(Time_2, mu_2, hold_df.Time, hold_df.friction);
-        start_hold_T(i) = hold_df.Time(I_hold(1));
-        end_hold_T(i) = hold_df.Time(I_hold(2));
-        start_hold_mu(i) = hold_df.friction(I_hold(1));
-        end_hold_mu(i) = hold_df.friction(I_hold(2));
-        start_hold_disp(i) = hold_df.LoadingPlattenDispHighGain(I_hold(1));
-        end_hold_disp(i) = hold_df.LoadingPlattenDispHighGain(I_hold(2));
-        plot(start_hold_T(i), start_hold_mu(i), 'o')
+        ylabel('friction (\mu)')
+        title('Pick start of hold')
+        ax = gca();
+        ylim_bottom = max(start_df.friction) - (max(start_df.friction) - min(end_df.friction))/4;
+        ylim([ylim_bottom, ax.YLim(2)])
+        clear Time_1 mu_1 I_start
+        [Time_1, mu_1] = ginput(1);
+        [~,~, I_start] = after_ginput(Time_1, mu_1, start_df.Time, start_df.friction);
+
+        subplot(1,3,2)
+        plot(file_df.Time(end_hold_indices), file_df.friction(end_hold_indices))
+        xlabel('Time (s)')
+        ylabel('friction (\mu)')
         hold on
+        title('Pick end of hold')
+        ax = gca();
+        ylim_top = min(end_df.friction) + (max(start_df.friction) - min(end_df.friction))/4;
+        ylim([ax.YLim(1), ylim_top])
+        clear Time_2 mu_2 I_end
+        [Time_2, mu_2] = ginput(1);
+        [~,~, I_end] = after_ginput(Time_2, mu_2, end_df.Time, end_df.friction);
+        
+        start_hold_T(i) = start_df.Time(I_start);
+        end_hold_T(i) = end_df.Time(I_end);
+        start_hold_mu(i) = start_df.friction(I_start);
+        end_hold_mu(i) = end_df.friction(I_end);
+        start_hold_index(i) = start_df.OG_Index(I_start);
+        end_hold_index(i) = end_df.OG_Index(I_end);
+
+        subplot(1,3,3)
+        plot(file_df.Time(hold_indices), file_df.friction(hold_indices))
+        hold on
+        plot(start_hold_T(i), start_hold_mu(i), 'o')
         plot(end_hold_T(i), end_hold_mu(i), 'square')
         hold off
     end
-    save(save_file + "hold_picks.mat", "end_hold_mu", "end_hold_T", "start_hold_mu", "start_hold_T", "start_hold_disp", "end_hold_disp", '-mat')
+    save(save_file + "hold_picks.mat", "end_hold_mu", "end_hold_index", "start_hold_mu", "start_hold_index", '-mat')
 end
